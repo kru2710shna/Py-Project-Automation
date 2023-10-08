af2d280031f6579a6dc284d6af812e0e957a9d50
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2023-10-08 15:25:18 -0700

    Instagram_Automate

---
+++ Instagram Automate App/Instagram_Automate.py
@@ -0,0 +1,22 @@
+from instabot import Bot
+
+bot = Bot()
+bot.login(username="user", password="<PASSWORD>")
+#bot.follow("Enter Username to follow")
+#bot.upload_photo("Enter Path", caption = "Your Choice")
+#bot.unfollow("Enter username to unfollow")
+#bot.send_message("Message Info" , ["Username1" , "Unsername2"])
+# To get list of folllowers 
+#followers = bot.get_user_followers("Your Username")
+#for follower in followers:
+    #print(bot.get_user_info(follower))
+
+# To get list of folllowings
+#followings = bot.get_user_followings("Your Username")
+#for following in followings:
+    #print(bot.get_user_info(following))
+
+
+
+
+
